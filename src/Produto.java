88504d61ce39dbbfd6cfc4a07dba83f5b2982fc5
Leon <cohenleonlbc@gmail.com>
2022-06-07 18:44:29 -0300

    comparable + toString + getsetters

--- src/Produto.java
+++ src/Produto.java
@@ -1,4 +1,4 @@
-public class Produto{
+public class Produto implements Comparable<Produto>{
     private int codigo;
     private String nome;
     private double valorUnitario;
@@ -9,5 +9,52 @@ public class Produto{
         this.nome = nome;
         this.valorUnitario = valorUnitario;
         this.estoque = estoque;
+    }
+
+    //Apenas getter, mudar o codigo quebraria arvore
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void vender(int qtd) {
+        if (this.estoque < qtd) {
+            //erro
+        } else {
+            this.estoque -= qtd;
+        }
+    }
+
+    @Override
+    public int compareTo(Produto o) {
+        if (this.codigo > o.getCodigo()){
+            return 1;
+        } else if (this.codigo < o.getCodigo()) {
+            return -1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "{ Codigo[" + this.getCodigo() + "] " +
+                this.getNome() + ": " +
+                this.getValorUnitario() + "$ -- " +
+                this.getEstoque() + "em estoque }";
     }
 }
