eb62d2b420c58f958a961490334f65772e25384e
Leon <cohenleonlbc@gmail.com>
2022-06-07 18:45:39 -0300

    generalization + comparable

--- src/ArvoreBinaria.java
+++ src/ArvoreBinaria.java
@@ -1,35 +1,40 @@
-public class ArvoreBinaria {
-    No raiz;
+import java.util.ArrayList;
+
+public class ArvoreBinaria<T extends Comparable<T>> {
+    No<T> raiz;
 
     public ArvoreBinaria(){
         this.raiz = null;
     }
 
-    public void deletaDado(int dado) { 
+    public void deletaDado(T dado) { 
         raiz = deletaRecursivo(raiz, dado); 
     } 
    
-    public No deletaRecursivo(No raiz, int dado)  { 
+    public No<T> deletaRecursivo(No<T> raiz, T dado)  { 
         if (raiz == null)  return raiz; 
    
-        if (dado < raiz.dado)
+        if (dado.compareTo(raiz.dado) < 0){
             raiz.esquerda = deletaRecursivo(raiz.esquerda, dado); 
-        else if (dado > raiz.dado)
+        }
+        else if (dado.compareTo(raiz.dado) > 0){
             raiz.direita = deletaRecursivo(raiz.direita, dado); 
+        }
         else  { 
-            if (raiz.esquerda == null) 
+            if (raiz.esquerda == null) {
                 return raiz.direita; 
-            else if (raiz.direita == null) 
+            }
+            else if (raiz.direita == null) {
                 return raiz.esquerda; 
-   
+            }
             raiz.dado = minimoValor(raiz.direita); 
             raiz.direita = deletaRecursivo(raiz.direita, raiz.dado); 
         } 
         return raiz; 
     } 
 
-    public int minimoValor(No raiz){
-        int minimo = raiz.dado;
+    public T minimoValor(No<T> raiz){
+        T minimo = raiz.dado;
 
         while(raiz.esquerda != null){
             minimo = raiz.esquerda.dado; 
@@ -38,48 +43,54 @@ public class ArvoreBinaria {
         return minimo; 
     }
 
-    public void inserir(int dado)  { 
+    public void inserir(T dado)  { 
         raiz = inserirRecursivo(raiz, dado); 
     }
 
-    public No inserirRecursivo(No raiz, int dado) { 
-      if (raiz == null) { 
-          raiz = new No(dado); 
-          return raiz; 
-      } 
-      if (dado < raiz.dado)
-          raiz.esquerda = inserirRecursivo(raiz.esquerda, dado); 
-      else if (dado > raiz.dado)
-          raiz.direita = inserirRecursivo(raiz.direita, dado); 
-      return raiz; 
+    public No<T> inserirRecursivo(No<T> raiz, T dado) { 
+        if (raiz == null) { 
+            raiz = new No<T>(dado); 
+            return raiz; 
+        } 
+        if (dado.compareTo(raiz.dado) < 0){
+            raiz.esquerda = inserirRecursivo(raiz.esquerda, dado);
+        }
+        else if (dado.compareTo(raiz.dado) > 0) {
+            raiz.direita = inserirRecursivo(raiz.direita, dado); 
+        }   
+        return raiz; 
     }
-  
 
-    public void emOrdem() { 
-        emOrdemRecursivo(raiz); 
+    public ArrayList<T> emOrdem(ArrayList<T> listaEmOrdem) { 
+        return emOrdemRecursivo(raiz, listaEmOrdem);
     } 
    
-    public void emOrdemRecursivo(No raiz) { 
+    public ArrayList<T> emOrdemRecursivo(No<T> raiz, ArrayList<T> listaEmOrdem) {
         if (raiz != null) { 
-            emOrdemRecursivo(raiz.esquerda); 
-            System.out.print(raiz.dado + " "); 
-            emOrdemRecursivo(raiz.direita); 
-        } 
+            emOrdemRecursivo(raiz.esquerda, listaEmOrdem); 
+            listaEmOrdem.add(raiz.dado);
+            emOrdemRecursivo(raiz.direita, listaEmOrdem);
+        }
+        return listaEmOrdem;
     }
 
-    public boolean pesquisar(int dado)  { 
+    public boolean pesquisar(T dado)  { 
         raiz = pesquisarRecursivo(raiz, dado); 
-        if (raiz!= null)
+        if (raiz!= null){
             return true;
-        else
+        }
+        else {
             return false;
+        }
     } 
 
-    public No pesquisarRecursivo(No raiz, int dado)  { 
-        if (raiz==null || raiz.dado==dado) 
+    public No<T> pesquisarRecursivo(No<T> raiz, T dado)  { 
+        if (raiz==null || raiz.dado.compareTo(dado) == 0) {
             return raiz; 
-        if (raiz.dado > dado) 
+        }   
+        if (raiz.dado.compareTo(dado) > 0){
             return pesquisarRecursivo(raiz.esquerda, dado); 
+        }
         return pesquisarRecursivo(raiz.direita, dado); 
     } 
 }
